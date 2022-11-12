bc09ac1ec77a0946389dd6d1fc0e90d1b8c504fc
Sangmesh Methre <SangmeshMetre>
2022-11-12 22:58:09 +0530

    Permutations of a string

---
+++ Algorithms/PermutationsOfString.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class PermutationsOfString
+    {
+       public static void Permute(string enteredString, int leftindex, int rightindex)
+        {
+            //If lesftIndex and rightIndex are Pointing to the same index of the EnteredString, Simple Print the EnteredString
+            if (leftindex==rightindex)
+            {
+                Console.WriteLine(enteredString);
+            }
+            //otherwise
+            else
+            {
+                //we start from the leftindex here 1st index go till the rightindex here endof the enteredstring to compute
+                for (int i= leftindex; i < rightindex; i++)
+                {
+                    //swapping the leftindex char with the i'th index char i.e current and the next to compute for the next string 
+                    enteredString = Swap(enteredString, leftindex, i);
+                }
+            }
+        }
+        public static string Swap(string eneteredString, int leftindex, int rightindex)
+        {
+            // Creating a character array named tempstring to perform swapping operation 
+            char[] tempstring = eneteredString.ToCharArray();
+
+            //create a temporary variable temp for swpping opartion 
+            //swap logic to swap LeftIndex to the Rightindex
+            char temp = tempstring[leftindex];
+            tempstring[leftindex] = tempstring [rightindex];
+            tempstring[rightindex] = temp;
+
+            // Converting tempString array back to string 
+            string toReturn = new string(tempstring);
+
+            //Finally return the string witch we Converted from the Array 
+            return toReturn;
+        }
+        public void Driver()
+        {
+            Console.WriteLine("Enter the string to find all of it's permutations :");
+            string enteredString = Console.ReadLine();
+            Console.WriteLine("Permutation of the string " + enteredString + "are :");
+            Permute(enteredString, 0, enteredString.Length - 1);
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t\tWelcome to Algorithms Program");
+            while (true)
+            {
+                Console.WriteLine("\n 1. Program to return all permutations of a string");
+
+                Console.WriteLine(" 0. Exit");
+                Console.Write("Enter your choice : ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        PermutationsOfString permute = new PermutationsOfString();
+                        permute.Driver();
+                        break;
+
+                    case 0:
+                        return;
+                    default:
+                        Console.WriteLine("Please choose the correct option");
+                        break;
+                }
+            }
+        }
+    }
+    }
+
