4d8e803656089de70ef70dc620b8cd571d289407
Sangmesh Methre <SangmeshMetre>
2022-11-12 23:55:47 +0530

    Customize Messege

---
+++ Alagorithm/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Alagorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t\tWelcome to Algorithms Program");
+            while (true)
+            {
+                Console.WriteLine("1. Customize message demonstration using String function and RegEx");
+                Console.WriteLine(" 0. Exit");
+                Console.Write("Enter your choice : ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        CustomizeMessage messageDemonstration = new CustomizeMessage();
+                        messageDemonstration.Message();
+                        break;
+                    case 0:
+                        return;
+                    default:
+                        Console.WriteLine("Please choose the correct option");
+                        break;
+                }
+            }
+        }
+    }
+}
