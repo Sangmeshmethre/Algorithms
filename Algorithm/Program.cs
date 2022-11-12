dc86f9fc6732d4f996249ff2e3d4bb743006c1fe
Sangmesh Methre <SangmeshMetre>
2022-11-12 23:32:14 +0530

    Prime Number

---
+++ Algorithm/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t\tWelcome to Algorithms Program");
+            while (true)
+            {
+                Console.WriteLine(" 1. Prime numbers in the range 0 - 1000");
+                Console.WriteLine(" 0. Exit");
+                Console.Write("Enter your choice : ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        PrimesInRange primesInRange = new PrimesInRange();
+                        primesInRange.Prime();
+                        break;
+                    case 0:
+                        return;
+                    default:
+                        Console.WriteLine("Please choose the correct option");
+                        break;
+                }
+            }
+        }
+    }
+}
