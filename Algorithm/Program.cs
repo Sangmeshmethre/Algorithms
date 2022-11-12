34b349dd5d45991c6fdf9a5748427d303bfb3bfd
Sangmesh Methre <SangmeshMetre>
2022-11-12 23:40:31 +0530

    Anagram and Palindrome

---
+++ Algorithm/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t\tWelcome to Algorithms Program");
+            while (true)
+            {
+                Console.WriteLine(" 1. Prime numbers in 0 - 1000 that are anagram and palindrome");
+                Console.WriteLine(" 0. Exit");
+                Console.Write("Enter your choice : ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        ExtendedPrime extendedPrime = new ExtendedPrime();
+                        extendedPrime.Prime();
+                        break;
+                    case 0:
+                        return;
+                    default:
+                        Console.WriteLine("Please choose the correct option");
+                        break;
+                }
+            }
+        }
+    }
+}
